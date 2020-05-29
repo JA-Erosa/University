ea4ae9758e1b7ea67bf18fb3f975f5668e3ef46b
JA-Erosa <53341133+JA-Erosa@users.noreply.github.com>
2020-05-29 12:22:12 -0500

    Update HuffmanQueen2.py

--- Python/HuffmanCoding/HuffmanQueen2.py
+++ Python/HuffmanCoding/HuffmanQueen2.py
@@ -1,13 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from IPython.display import display_html
-def display_side_by_side(*args):
-    html_str=''
-    for df in args:
-        html_str+=df.to_html()
-    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
-
 with open('song.txt','r') as file:
     lines = []
     for line in file:
